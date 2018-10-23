933eef6b84fd880b63c32991c4d2d7c4a5c84ca5
SathwikGunda <42731745+SathwikGunda@users.noreply.github.com>
2018-10-23 16:44:48 +0530

    commit

--- search.php
+++ search.php
@@ -52,7 +52,7 @@
 
             <!--Grid column-->
             <div class="col-md-9 mx-auto mb-5">
-
+                
                 <!--Card-->
                 <div class="card mb-4 wow fadeIn">
 
@@ -65,44 +65,47 @@
                                 <h5 class="mt-0 font-weight-bold">Hospital Name 1</h5>
                                 <p class="grey-text mb-1">Sri Vani Nilayam, Opp To Nizampet X Roads Plot No - 177, Sardar Patel Nagar, Hyderabad, Telangana 500090</p>
                                 <ul class="list-inline mb-0">
-                                    <li class="list-inline-item mr-3">
-                                        <a href="#" id="departments">                                                   <i class="fa fa-building-o"></i>
-                                            <small>1 Department(s)</small>
-                                        </a>
+                                    <li class="list-inline-item mr-3 depts-name">
+                                        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style="padding:0px;font-size:15px;">
+                                            <i class="fa fa-building-o"></i>
+                                            <small>2 Department(s)</small>
+                                        </button>
                                     </li>
                                     <li class="list-inline-item mr-3">
                                         <i class="fa fa-user-md"></i>
                                         <small>2 Doctor(s)</small>
                                     </li>
                                 </ul>
+                                <div class="accordion" id="accordionExample">
+                                    <div class="card z-depth-0">
+                                        <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                                            <div class="card-body">
+                                                <table class="table table-bordered">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>S.No</th>
+                                                            <th>Department Name</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr>
+                                                            <td>1</td>
+                                                            <td>Addxxxxxx</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>2</td>
+                                                            <td>Subxxxxxxx</td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
                         
-                        <div class="select-dept mdb-color darken-6">
-                            <form method="" action="">
-                                <div class="row text-white mx-auto">
-                                    <div class="col-md-2">
-                                        <h5 class="mt-1">Departments</h5>
-                                    </div>
-                                    <div class="col-md-8">
-                                        <div class="form-group mb-0">
-                                            <select class="form-control">
-                                                <option value="" disabled selected>Select Department</option>
-                                                <option value="1">Option 1</option>
-                                                <option value="2">Option 2</option>
-                                                <option value="3">Option 3</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-2 text-right">
-                                        <a href="#" id="select-dept-close" class="text-white">
-                                            <i class="fa fa-times fa-1x mt-2" aria-hidden="true"></i>
-                                        </a>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-
                     </div>
 
                 </div>
@@ -120,44 +123,47 @@
                                 <h5 class="mt-0 font-weight-bold">Hospital Name 1</h5>
                                 <p class="grey-text mb-1">Sri Vani Nilayam, Opp To Nizampet X Roads Plot No - 177, Sardar Patel Nagar, Hyderabad, Telangana 500090</p>
                                 <ul class="list-inline mb-0">
-                                    <li class="list-inline-item mr-3">
-                                        <a href="#" id="departments">                                                   <i class="fa fa-building-o"></i>
-                                            <small>1 Department(s)</small>
-                                        </a>
+                                    <li class="list-inline-item mr-3 depts-name">
+                                        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseOne" style="padding:0px;font-size:15px;">
+                                            <i class="fa fa-building-o"></i>
+                                            <small>2 Department(s)</small>
+                                        </button>
                                     </li>
                                     <li class="list-inline-item mr-3">
                                         <i class="fa fa-user-md"></i>
                                         <small>2 Doctor(s)</small>
                                     </li>
                                 </ul>
+                                <div class="accordion" id="accordionExample">
+                                    <div class="card z-depth-0">
+                                        <div id="collapseTwo" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                                            <div class="card-body">
+                                                <table class="table table-bordered">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>S.No</th>
+                                                            <th>Department Name</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr>
+                                                            <td>1</td>
+                                                            <td>Addxxxxxx</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>2</td>
+                                                            <td>Subxxxxxxx</td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
                         
-                        <div class="select-dept mdb-color darken-6">
-                            <form method="" action="">
-                                <div class="row text-white mx-auto">
-                                    <div class="col-md-2">
-                                        <h5 class="mt-1">Departments</h5>
-                                    </div>
-                                    <div class="col-md-8">
-                                        <div class="form-group mb-0">
-                                            <select class="form-control">
-                                                <option value="" disabled selected>Select Department</option>
-                                                <option value="1">Option 1</option>
-                                                <option value="2">Option 2</option>
-                                                <option value="3">Option 3</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-2 text-right">
-                                        <a href="#" id="select-dept-close" class="text-white">
-                                            <i class="fa fa-times fa-1x mt-2" aria-hidden="true"></i>
-                                        </a>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-
                     </div>
 
                 </div>
@@ -175,49 +181,109 @@
                                 <h5 class="mt-0 font-weight-bold">Hospital Name 1</h5>
                                 <p class="grey-text mb-1">Sri Vani Nilayam, Opp To Nizampet X Roads Plot No - 177, Sardar Patel Nagar, Hyderabad, Telangana 500090</p>
                                 <ul class="list-inline mb-0">
-                                    <li class="list-inline-item mr-3">
-                                        <a href="#" id="departments">                                                   <i class="fa fa-building-o"></i>
-                                            <small>1 Department(s)</small>
-                                        </a>
+                                    <li class="list-inline-item mr-3 depts-name">
+                                        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseOne" style="padding:0px;font-size:15px;">
+                                            <i class="fa fa-building-o"></i>
+                                            <small>2 Department(s)</small>
+                                        </button>
                                     </li>
                                     <li class="list-inline-item mr-3">
                                         <i class="fa fa-user-md"></i>
                                         <small>2 Doctor(s)</small>
                                     </li>
                                 </ul>
+                                <div class="accordion" id="accordionExample">
+                                    <div class="card z-depth-0">
+                                        <div id="collapseThree" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                                            <div class="card-body">
+                                                <table class="table table-bordered">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>S.No</th>
+                                                            <th>Department Name</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr>
+                                                            <td>1</td>
+                                                            <td>Addxxxxxx</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>2</td>
+                                                            <td>Subxxxxxxx</td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
                         
-                        <div class="select-dept mdb-color darken-6">
-                            <form method="" action="">
-                                <div class="row text-white mx-auto">
-                                    <div class="col-md-2">
-                                        <h5 class="mt-1">Departments</h5>
-                                    </div>
-                                    <div class="col-md-8">
-                                        <div class="form-group mb-0">
-                                            <select class="form-control">
-                                                <option value="" disabled selected>Select Department</option>
-                                                <option value="1">Option 1</option>
-                                                <option value="2">Option 2</option>
-                                                <option value="3">Option 3</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-2 text-right">
-                                        <a href="#" id="select-dept-close" class="text-white">
-                                            <i class="fa fa-times fa-1x mt-2" aria-hidden="true"></i>
-                                        </a>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-
                     </div>
 
                 </div>
                 <!--/.Card-->
+                
+                <!--Card-->
+                <div class="card mb-4 wow fadeIn">
 
+                    <!--Card content-->
+                    <div class="card-body">
+
+                        <div class="media d-block d-md-flex mt-3">
+                            <img class="d-flex mb-3 mx-auto z-depth-1" src="https://mdbootstrap.com/img/Photos/Avatars/img (30).jpg" alt="Generic placeholder image" style="width: 100px;">
+                            <div class="media-body text-center text-md-left ml-md-3 ml-0">
+                                <h5 class="mt-0 font-weight-bold">Hospital Name 1</h5>
+                                <p class="grey-text mb-1">Sri Vani Nilayam, Opp To Nizampet X Roads Plot No - 177, Sardar Patel Nagar, Hyderabad, Telangana 500090</p>
+                                <ul class="list-inline mb-0">
+                                    <li class="list-inline-item mr-3 depts-name">
+                                        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="true" aria-controls="collapseOne" style="padding:0px;font-size:15px;">
+                                            <i class="fa fa-building-o"></i>
+                                            <small>2 Department(s)</small>
+                                        </button>
+                                    </li>
+                                    <li class="list-inline-item mr-3">
+                                        <i class="fa fa-user-md"></i>
+                                        <small>2 Doctor(s)</small>
+                                    </li>
+                                </ul>
+                                <div class="accordion" id="accordionExample">
+                                    <div class="card z-depth-0">
+                                        <div id="collapseFour" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                                            <div class="card-body">
+                                                <table class="table table-bordered">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>S.No</th>
+                                                            <th>Department Name</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr>
+                                                            <td>1</td>
+                                                            <td>Addxxxxxx</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>2</td>
+                                                            <td>Subxxxxxxx</td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                </div>
+                <!--/.Card-->
 
             </div>
             <!--Grid column-->
@@ -231,15 +297,5 @@
 </main>
 <!--Main layout-->
 
-<?php include('footer.php'); ?>
 
-<script>
-    $(document).ready(function(){
-        $("#departments").click(function(){
-            $(".select-dept").toggle();
-        });
-        $("#select-dept-close").click(function(){
-            $(".select-dept").hide();
-        });
-    });
-</script>
+<?php include('footer.php'); ?>
