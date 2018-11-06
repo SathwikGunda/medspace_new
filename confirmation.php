c05877247c9357d18b0c9d0506b384790d9e9844
SathwikGunda <42731745+SathwikGunda@users.noreply.github.com>
2018-11-06 19:30:17 +0530

    commit

--- confirmation.php
+++ confirmation.php
@@ -1,18 +1,106 @@
 <?php include('header.php'); ?>
+<style>
+    table.table td{
+        padding-top: 10px;
+        padding-bottom: 0px;
+    }
+    table.table td:first-child{
+        font-weight: 700;
+    }
 
+</style>
 <!--Main layout-->
-<main class="mt-5 pt-5 mb-5 pb-5">
+<main class="">
     <div class="container">
 
         <!--Section: Post-->
-        <section class="mt-5 mb-5 pt-5 pb-5">
+        <section class="mt-5 mb-5">
+    
+            <!--Grid row-->
+            <div class="row">
+
+                <!--Grid column-->
+                <div class="col-md-6">
+            
+                    <div class="card mb-5">
+                        <div class="card-header font-weight-bold">Address</div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-borderless">
+                                    <tbody>
+                                        <tr>
+                                            <td><b>Address</b></td>
+                                            <td>xxxxxxxxx</td>
+                                        </tr>
+                                        <tr>
+                                            <td><b>City</b></td>
+                                            <td>xxxxxxxxx</td>
+                                        </tr>
+                                        <tr>
+                                            <td><b>State</b></td>
+                                            <td>xxxxxxxxx</td>
+                                        </tr>
+                                        <tr>
+                                            <td><b>Country</b></td>
+                                            <td>xxxxxxxxx</td>
+                                        </tr>
+                                        <tr>
+                                            <td><b>Zipcode</b></td>
+                                            <td>xxxxxxxxx</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+            
+                </div>
+                
+                <!--Grid column-->
+                <div class="col-md-6">
+            
+                    <div class="card mb-5">
+                        <div class="card-header font-weight-bold">Patient Details</div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-borderless">
+                                    <tbody>
+                                        <tr>
+                                            <td><b>Name</b></td>
+                                            <td>xxxxxxxxx</td>
+                                        </tr>
+                                        <tr>
+                                            <td><b>Mobile Number</b></td>
+                                            <td>xxxxxxxxx</td>
+                                        </tr>
+                                        <tr>
+                                            <td><b>Email Id</b></td>
+                                            <td>xxxxxxxxx</td>
+                                        </tr>
+                                        <tr>
+                                            <td><b>Gender</b></td>
+                                            <td>xxxxxxxxx</td>
+                                        </tr>
+                                        <tr>
+                                            <td><b>Age</b></td>
+                                            <td>xxxxxxxxx</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+            
+                </div>
+                
+            </div>
 
             <!--Grid row-->
             <div class="row">
 
                 <!--Grid column-->
                 <div class="col-md-8 mx-auto text-center">
-
+                    
                     <img src="img/confirm-booking.png" height="100px">
                     <h1>Booking Confirmed</h1>
 
